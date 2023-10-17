968e91c9647e2c7f3582bfd03b5f2f4114deb2ee
dicarne <dicarne@zhishudali.ink>
2023-10-17 18:57:08 +0800

    添加项目文件。

---
+++ Form1.cs
@@ -0,0 +1,17 @@
+namespace Glass
+{
+    public partial class Grass : Form
+    {
+        public Grass()
+        {
+            InitializeComponent();
+            this.BackColor = Color.FromArgb(1, 2, 3);
+            this.TransparencyKey = Color.FromArgb(1, 2, 3);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
